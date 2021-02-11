1ebb9fa8b7c1c3ca5fe2b7d09b25f8346a5e040b
HelPek232323 <67285722+HelPek232323@users.noreply.github.com>
2021-02-11 14:57:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Заголовок страницы</title>
+</head>
+<body>
+
+<p><a href="files/files.rar">Скачать файлы</a></p>
+<p><a href="files/WinRar.exe">Скачать WinRAR</a></p>
+
+</body>
+</html>
